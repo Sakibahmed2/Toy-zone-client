a67c588dea440719cc3256bfc05567127830d43e
Sakibahmed2 <mauijaloskor@gmail.com>
2023-05-17 21:45:51 +0600

    banner done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <div className="hero mt-12 ">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src="https://cdn.pixabay.com/photo/2016/10/09/00/18/star-wars-1724901_960_720.jpg" className="max-w-lg rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">We make your children happier with the best toys</h1>
+                        <p className="py-6">We have products for all ages. Our assistants always help you to do right choice. A kids toy shop is a store that specializes in selling toys and games designed specifically for children.</p>
+                        <button className="my-btn">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
             <div className="navbar lg:flex lg:justify-between items-center">
                 <div>
                     <img className='w-28' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbyQtLI870ha38kUm2BvywcisdcrHvzKSH3Xy5qtymjA&s" alt="" />
-                    <h2 className="normal-case font-bold text-4xl ml-28 text-amber-700 hover:text-amber-600">Toy zone</h2>
+                    <h2 className="normal-case font-bold text-4xl ml-28 my-text hover:text-amber-600">Toy zone</h2>
                 </div>
                 <div className='text-lg flex font-semibold'>
                     <Link to='/'>Home</Link>
